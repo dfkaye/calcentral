7edea22942159ba2df30d3e679e17c8a03be9b1c
david kaye <dfkaye@yahoo.com>
2016-01-15 16:26:22 -0800

    SISRP-1077:WIP:model-binding;view-toggling;

--- src/assets/javascripts/angular/controllers/widgets/profile/languagesSectionController.js
+++ src/assets/javascripts/angular/controllers/widgets/profile/languagesSectionController.js
@@ -6,74 +6,75 @@ var angular = require('angular');
 /**
  * Language section controller
  */
-angular.module('calcentral.controllers').controller('LanguagesSectionController', function(profileFactory, $scope, $q) {
-  var levelMapping = {
-    native: 'native',
-    teach: 'teacher',
-    translate: 'translator'
-  };
-
-  var sortLanguageCodes = function(codes) {
-    var sortedCodes = codes.sort(function(a, b) {
-      return a.descr > b.descr;
-    });
-
-    return sortedCodes;
-  };
-
-  var fetchLanguageCodes = function() {
-    // i like the promise api here...
-    return profileFactory.getLanguageCode().then(function(data) {
-      var languageCodes = sortLanguageCodes(data.data.feed.accomplishments);
-      // console.warn('languagesController');
-      angular.extend($scope, {
-        languageCodes: {
-          content: languageCodes,
-          selectedCode: ''
-        }
-      });
-      // console.warn($scope.languageCodes.content);
-    });
-  };
-
-  var openEditor = function() {
-    $scope.editorEnabled = true;
-  };
+angular.module('calcentral.controllers').controller('LanguagesSectionController', function(apiService, profileFactory, $scope, $q) {
+  // model and state
+  angular.extend($scope, {
+    addingItem: false,
+    currentObject: {},
+    emptyObject: {},
+    errorMessage: '',
+    isSaving: false,
+    items: {
+      content: [],
+      editorEnabled: false,
+      loaded: false
+    },
+    languageCodes: {
+      content: [],
+      loaded: false,
+      selectedCode: ''
+    }
+  });
 
   var closeEditor = function() {
-    $scope.adding = false;
-    $scope.errorMessage = false;
-    $scope.editorEnabled = false;
+    $scope.addingItem = false;
+
+    apiService.profile.closeEditor($scope);
   };
 
-  $scope.showEdit = function() {
-    if (!$scope.languageCodes) {
-      // lazy request codes once
-      fetchLanguageCodes().then(openEditor);
-    } else {
-      openEditor();
-    }
+  $scope.showEdit = function(item) {
+    apiService.profile.showEdit($scope, item);
+    // console.warn($scope.currentObject);
   };
 
   $scope.showAdd = function() {
-    $scope.adding = true;
-    $scope.showEdit();
+    $scope.addingItem = true;
+
+    apiService.profile.showAdd($scope, {
+      // code: $scope.languageCodes.content[0].accomplishment
+    });
   };
 
   $scope.cancelEdit = function() {
     closeEditor();
   };
 
-  $scope.saveItem = function() {
+  $scope.saveItem = function(item) {
+  console.log(item);/*  {
+      languageCode: currentObject.data.code,
+      isNative: currentObject.data.native,
+      isTranslateToNative: currentObject.data.translate,
+      isTeachLanguage: currentObject.data.teach,
+      speakProf: currentObject.data.speakingProficiency.description,
+      readProf: currentObject.data.readingingProficiency.description,
+      teachLang: currentObject.data.writingProficiency.description
+    }*/
+    $scope.isSaving = true;
     // console.log($scope.languageCodes.selectedCode)
     // console.warn(data);
-    $scope.errorMessage = 'there was an error';
+    $scope.errorMessage = 'this is a fake error message hand-crafted at the keyboard using an over-abundance of text characters';
     // closeEditor();
   };
 
-  $scope.deleteItem = function() {
-    // console.warn(data);
+  $scope.deleteItem = function(item) {
+    console.warn(item);
     closeEditor();
+  };
+
+  var levelMapping = {
+    native: 'native',
+    teach: 'teacher',
+    translate: 'translator'
   };
 
   var parseLevels = function(languages) {
@@ -95,25 +96,45 @@ angular.module('calcentral.controllers').controller('LanguagesSectionController'
     var person = data.data.feed.student;
     var languages = parseLevels(person.languages);
     angular.extend($scope, {
-      languages: {
+      items: {
         content: languages,
-        supported: true
-      },
-      currentObject: {},
-      editorEnabled: false,
-      errorMessage: false,
-      adding: false,
-      isSaving: false
+        loaded: true
+      }
     });
   };
 
   var getPerson = profileFactory.getPerson().then(parsePerson);
 
   var loadInformation = function() {
+    $scope.isLoading = true;
     $q.all(getPerson).then(function() {
       $scope.isLoading = false;
     });
   };
 
-  loadInformation();
+  var sortLanguageCodes = function(codes) {
+    var sortedCodes = codes.sort(function(a, b) {
+      return a.descr > b.descr;
+    });
+    return sortedCodes;
+  };
+
+  var fetchLanguageCodes = function() {
+    // i like the promise api here.
+    return profileFactory.getLanguageCode().then(function(data) {
+      var languageCodes = sortLanguageCodes(data.data.feed.accomplishments);
+      angular.extend($scope, {
+        languageCodes: {
+          content: languageCodes,
+          loaded: true,
+          selectedCode: ''
+        }
+      });
+      // console.warn($scope.languageCodes.content);
+    });
+  };
+
+  // call fetch() once, return a promise for loadInformation
+  // loadInformation can be called independently for any subsequent refresh
+  fetchLanguageCodes().then(loadInformation);
 });
